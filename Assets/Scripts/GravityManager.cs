15a0be2e52542e3290ac64a75f382a4ad8255e82
Anthony Ramirez <anthonycramirez@gmail.com>
2018-10-14 15:11:47 -0700

    Updated the GravityManager to now longer have a dead zone for testing how it feels.

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -34,7 +34,6 @@ public class GravityManager: MonoBehaviour, IGameState
 			{
 				float xx;
 				float yy;
-				float zz;
 
 				#if UNITY_EDITOR
 				xx = Mathf.Sin (Camera.main.transform.localEulerAngles.z*Mathf.Deg2Rad) * GRAV_SPEED;
@@ -42,20 +41,12 @@ public class GravityManager: MonoBehaviour, IGameState
 				Physics.gravity = new Vector3(xx,-yy);
 
 				#else
-				xx = Input.gyro.gravity.x;
-				yy = Input.gyro.gravity.y;
-				zz = Input.gyro.gravity.z;
-			
-				if (xx > -.2f && xx < .2f && yy > -.2f && yy < .2f && (zz < -.75f || zz > .75f))
-				{
-					Physics.gravity = new Vector3 (0.0f, -1.0f, 0.0f) * GRAV_SPEED;
-				} else
-				{
-					xx = Input.gyro.gravity.x * GRAV_SPEED;
-					yy = Input.gyro.gravity.y * GRAV_SPEED;
 				
-					Physics.gravity = new Vector3 (xx, yy);
-				}
+				xx = Input.gyro.gravity.x * GRAV_SPEED;
+				yy = Input.gyro.gravity.y * GRAV_SPEED;
+				
+				Physics.gravity = new Vector3 (xx, yy);
+				
 				#endif
 			}
 		}
