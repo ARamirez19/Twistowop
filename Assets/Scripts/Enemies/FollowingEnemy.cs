61cc4458ccd18d1a5d53544363e3fc2bfcc68098
cdeao <cmdeao16@gmail.com>
2019-02-27 17:37:15 -0600

    Created the "Floaty Following Enemy" prefab for placement within levels. Currently uses the "CoinSprite" as a placeholder sprite and will need to be updated.

---
+++ Assets/Scripts/Enemies/FollowingEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingEnemy : EnemyController
+{
+	public GameObject player;
+	[SerializeField]
+	private float movementSpeed = 5.0f;
+	private Vector2 enemyDirection;
+    // Start is called before the first frame update
+    protected override void ExtraStart()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(state == GameState.e_GAMESTATE.PLAYING)
+		{
+			enemyDirection = player.transform.position - transform.position;
+			enemyDirection = enemyDirection.normalized;
+			transform.Translate(enemyDirection * movementSpeed * Time.deltaTime);
+		}
+    }
+}
