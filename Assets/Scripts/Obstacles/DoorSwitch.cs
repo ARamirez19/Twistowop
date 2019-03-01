629911ced5a7829c68d10314d4c98c759a1e80a7
cdeao <cmdeao16@gmail.com>
2019-02-28 19:59:12 -0600

    Created Doors/Buttons prefabs and the Sticky Walls prefab. Prefabs currently use placeholder sprites. Added a new tag for "Door" within the project.

---
+++ Assets/Scripts/Obstacles/DoorSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour
+{
+	private GameObject door;
+	private Renderer doorRenderer;
+	private Collider2D doorCollider;
+	private bool switchEnabled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+		door = GameObject.FindGameObjectWithTag("Door");
+		doorRenderer = door.GetComponent<Renderer>();
+		doorCollider = door.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.collider.tag == "Player" && switchEnabled == false)
+		{
+			StartCoroutine(DoorTimer());
+		}
+	}
+
+	IEnumerator DoorTimer()
+	{
+		switchEnabled = true;
+		doorRenderer.enabled = false;
+		doorCollider.enabled = false;
+		yield return new WaitForSeconds(5);
+		switchEnabled = false;
+		doorRenderer.enabled = true;
+		doorCollider.enabled = true;
+	}
+}
---
+++ Assets/Scripts/Obstacles/StickyWalls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyWalls : MonoBehaviour
+{
+	[SerializeField]
+	private bool objectStuck = false;
+	[SerializeField]
+	private bool eligibleStick = true;
+	private Rigidbody2D otherRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		otherRigidbody = other.gameObject.GetComponent<Rigidbody2D>();
+		if(objectStuck == false && eligibleStick == true)
+		{
+			objectStuck = true;
+			otherRigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
+			StartCoroutine(StuckTimer(otherRigidbody));
+		}
+	}
+
+	IEnumerator StuckTimer(Rigidbody2D otherRigidbody)
+	{
+		yield return new WaitForSeconds(2.0f);
+		otherRigidbody.constraints = RigidbodyConstraints2D.None;
+		objectStuck = false;
+		eligibleStick = false;
+		yield return new WaitForSeconds(2);
+		eligibleStick = true;
+	}
+}
