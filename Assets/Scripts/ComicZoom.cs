1977870edd0d8f11c11bef31509fd37ecfb94953
Christopher Price <cspprice@gmail.com>
2019-04-03 17:49:37 -0700

    Added multiple comic panels that can be switched to

--- Assets/Scripts/ComicZoom.cs
+++ Assets/Scripts/ComicZoom.cs
@@ -6,26 +6,89 @@ using UnityEngine.UI;
 public class ComicZoom : MonoBehaviour
 {
     [SerializeField]
-    private GameObject image; // The canvas
+    private GameObject image; // The image to be scaled
     [SerializeField]
     private float zoomSpeed = 0.5f; // The rate of change of the canvas scale factor
     [SerializeField]
     private float minScale = 0.6f; // The minimum scale the image will scale to
     [SerializeField]
     private float maxScale = 2.0f; // The maximum scale the image will scale to
+    [SerializeField]
+    private Sprite[] comics;
+
+    public int currentComic = 0;
 
     private ScrollRect scrollRect;
 
-    private void Start()
+    private Vector2 startPos;
+    private Vector2 direction;
+    private bool directionChosen;
+    private bool resizing = false;
+
+    void Start()
     {
+        image.GetComponent<Image>().sprite = comics[currentComic];
         scrollRect = GetComponent<ScrollRect>();
     }
 
     void Update()
     {
-        // If there are two touches on the device...
-        if (Input.touchCount == 2)
+        Debug.Log(Input.touchCount);
+        // If there is one touch on the device
+        if(Input.touchCount == 1)
         {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                // Record initial touch position.
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    directionChosen = false;
+                    break;
+
+                // Determine direction by comparing the current touch position with the initial one.
+                case TouchPhase.Moved:
+                    direction = touch.position - startPos;
+                    break;
+
+                // Report that a direction has been chosen when the finger is lifted.
+                case TouchPhase.Ended:
+                    directionChosen = true;
+                    break;
+            }
+            if(directionChosen && !resizing)
+            {
+                if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                {
+                    scrollRect.vertical = false;
+                    // Check if the swipe if left and update comic page
+                    if (direction.x < -50)
+                    {
+                        if (currentComic < comics.Length - 1)
+                        {
+                            currentComic++;
+                            image.transform.localScale = new Vector3(1, 1, 1);
+                            image.GetComponent<Image>().sprite = comics[currentComic];
+                        }
+                    }
+                    // Check if the swipe is right and update comic page
+                    else if (direction.x > 50)
+                    {
+                        if (currentComic > 0)
+                        {
+                            currentComic--;
+                            image.transform.localScale = new Vector3(1, 1, 1);
+                            image.GetComponent<Image>().sprite = comics[currentComic];
+                        }
+                    }
+                }
+            }
+            directionChosen = false;
+        }
+        // If there are two touches on the device
+        else if (Input.touchCount == 2)
+        {
+            resizing = true;
             //Turn off vertical scrolling of the scroll rect
             scrollRect.vertical = false;
 
@@ -49,11 +112,18 @@ public class ComicZoom : MonoBehaviour
 
             // Clamp the image to the maximum size
             image.transform.localScale = new Vector3(Mathf.Clamp(image.transform.localScale.x, minScale, maxScale), Mathf.Clamp(image.transform.localScale.y, minScale, maxScale));
+            StartCoroutine(PauseSwipe());
         }
         else
         {
             // Restore Vertical Scrolling
             scrollRect.vertical = true;
         }
+    }
+
+    IEnumerator PauseSwipe()
+    {
+        yield return new WaitForSeconds(0.5f);
+        resizing = false;
     }
 }
