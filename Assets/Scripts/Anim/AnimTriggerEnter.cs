00b9a163c15ad86d2b0ad1b40f608d8bd2b07f6e
cmdeao <cmdeao16@gmail.com>
2019-04-08 13:45:35 -0500

    Integrated the updated "Carrot" collectibles prefab, new "Goal" prefab with animations, new "Portals" prefab with animations, new "Sticky Wall" prefab with animations and resizing script, new "Do Not Touch" prefab with animations, and new "Cubark" prefab with first round of animations. Integrated the "Nice Vibrations" framework for future use of haptics.

---
+++ Assets/Scripts/Anim/AnimTriggerEnter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTriggerEnter : MonoBehaviour
+{
+
+    public Collider2D col2D;
+    public Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col2D = GetComponent<Collider2D>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            myAnim.SetTrigger("myTrigger");
+        }
+    }
+}
---
+++ Assets/Scripts/sticky_resize.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class sticky_resize : MonoBehaviour
+{
+    public float Length;
+
+    public ParticleSystem lightning;
+    public ParticleSystem zaps;
+    public SpriteRenderer magnet;
+    public CapsuleCollider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        magnet.size = new Vector2(Length + 2, 1.8f);
+
+        var shape = lightning.shape;
+        shape.radius = Length;
+
+        var sparkShape = zaps.shape;
+        sparkShape.radius = Length;
+
+        //var colSize = col.size;
+        col.size = new Vector2((Length * 2) + 4, 2f);
+        
+    }
+}
