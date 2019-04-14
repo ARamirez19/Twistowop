5c9456d9b9ac366dc47e84e421f4f947aeb7cc5f
Tim Pryor <tim@timpryor.com>
2019-04-14 01:11:43 -0700

    Audio Commit:
    -Bumper audio
    -Test music system (sounds terrible because of stand-in music)
    -Shooter push pulse (don't know name) enemy audio -> bug, shooters use the same prefab. Needs to change.

--- Twistowop_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ Twistowop_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,7 +17,9 @@ namespace AK
         static const AkUniqueID PLAY_BALLENEMY_ATTACK = 506762798U;
         static const AkUniqueID PLAY_BALLENEMY_BARK = 789229116U;
         static const AkUniqueID PLAY_BALLENEMY_BEEP = 738307890U;
-        static const AkUniqueID PLAY_MAINMENUTHEME = 2251157239U;
+        static const AkUniqueID PLAY_BUMPER_HIT = 1705323465U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_RAYGUNSHOOTER_FIRELOOP = 2660315473U;
         static const AkUniqueID STOP_BALLENEMY_AMBIENCE = 1677540204U;
     } // namespace EVENTS
 
@@ -33,6 +35,17 @@ namespace AK
                 static const AkUniqueID INACTIVE = 3163453698U;
             } // namespace STATE
         } // namespace BALLENEMY_AMBIENCE
+
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MAINMENU = 3604647259U;
+                static const AkUniqueID ZONE_1 = 2643994087U;
+            } // namespace STATE
+        } // namespace MUSIC
 
     } // namespace STATES
 
@@ -50,6 +63,8 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MX = 1685527054U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
