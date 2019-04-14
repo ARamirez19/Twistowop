099b0ec6ec85ce6a8a50c054ba7e68ab81e57d57
Tim Pryor <tim@timpryor.com>
2019-04-14 11:44:00 -0700

    Added:
    -Carrot pickup audio
    -Ship level ambience
    -Swapped out sample music for sam's new sample (still not finals. Music will sound bad and have gaps between start and stops)

--- Assets/Scripts/Collectables/BaseCollectable.cs
+++ Assets/Scripts/Collectables/BaseCollectable.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class BaseCollectable : MonoBehaviour
 {
+    public string audioEventID = "";
+
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (string.Compare(other.tag, "Player") == 0)
         {
             LevelManager.GetInstance().CurrentCollectableCount++;
+            if(audioEventID != "")
+                AkSoundEngine.PostEvent(audioEventID, gameObject);
             Destroy(this.gameObject);
         }
     }
